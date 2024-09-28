292b24651e3bb53f8966053df3426d02db509c55
mihirathale98 <athale.m@northeastern.edu>
2024-09-28 15:55:35 -0400

    some stuff

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import json
+from src.scraper import scrape_data
+
+# Set the title of the app
+st.title("Market Hypothesis Validation")
 
 with st.form("my_form"):
     st.write("Inside the form")
@@ -34,22 +39,15 @@ with st.form("my_form"):
     submitted = st.form_submit_button("Submit")
 
     if submitted:
-        # Output the form inputs
-        st.write("Company Website:", company_website)
-        st.write("Uploaded Files:", uploaded_files)
-        st.write("Company Name:", company_name)
-        st.write("Location:", location)
-        st.write("Company Description:", company_description)
-        st.write("Product/Service Description:", product_description)
-        st.write("Main Message:", main_message)
-        st.write("Target Customer:", target_customer)
-        st.write("Customer Type - B2B:", customer_b2b)
-        st.write("Customer Type - B2C:", customer_b2c)
-        st.write("Customer Type - B2B2C:", customer_b2b2c)
-        st.write("Market Type - Physical Product:", market_physical)
-        st.write("Market Type - Digital Product:", market_digital)
-        st.write("Market Type - Service:", market_service)
-        st.write("Selected Color:", selected_color)
 
-# Outside the form
-st.write("Outside the form - TBD")
+        company_website_data = scrape_data(company_website)
+        st.write(company_website_data)
+        company_data = {'company_website': company_website, 'company_name': company_name, 'location': location,
+                        'company_description': company_description, 'product_description': product_description,
+                        'main_message': main_message, 'target_customer': target_customer, 'customer_b2b': customer_b2b,
+                        'customer_b2c': customer_b2c, 'customer_b2b2c': customer_b2b2c, 'market_physical': market_physical,
+                        'market_digital': market_digital, 'market_service': market_service, 'logo_upload': logo_upload, 
+                        'company_website_data': company_website_data}
+
+        with open("company_data.json", "w") as f:
+            json.dump(company_data, f)
