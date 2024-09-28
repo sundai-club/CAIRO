d9adc42ab6204150aac6af76d15526152ffd7146
Aleks Jakulin <aleks@ganxy.com>
2024-09-28 18:39:16 -0400

    more jamba

---
+++ jamba/streaming.jamba.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from ai21 import AI21Client
+from ai21.models.chat import UserMessage
+import os 
+import html
+import csv
+
+rr = ""
+envs={}
+for row in csv.reader(open(".env"),delimiter='='):
+        envs[row[0]]=row[1]
+
+api_key=envs["JAMBA_KEY"]
+client = AI21Client(api_key=api_key)
+
+st.title("Chat Interface with Custom Logic")
+
+# Initialize session state to store the chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display previous chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if user_input := st.chat_input("Type your message"):
+    # Add the user's message to the chat history
+    st.session_state.messages.append({"role": "user", "content": user_input})
+
+    # Display the user's message
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+    messages = [
+        UserMessage(
+            content="translate user's message to spanish. Message:"+user_input
+        )
+    ]
+
+    response = client.chat.completions.create(
+        model="jamba-1.5-large",
+        messages=messages,
+        top_p=1.0, # Setting to 1 encourages different responses each call.
+        stream=True,
+    )
+    
+    rr = ""
+    # Display the assistant's response
+    def create_generator(response):
+        global rr
+        while True:
+            for chunk in response:
+                c = chunk.choices[0].delta.content
+                if c:
+                    yield(html.unescape(c))
+                    rr += str(chunk.choices[0].delta.content)
+                if chunk.choices[0].finish_reason=="stop":
+                    return 
+    st.chat_message("assistant").write_stream(create_generator(response))
+    
+    # Add the assistant's response to the chat history
+    st.session_state.messages.append({"role": "assistant", "content": rr})
